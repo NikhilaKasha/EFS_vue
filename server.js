5c90cd35b4f56259315fb68f78684f187763e03a
NikhilaKasha <54554903+NikhilaKasha@users.noreply.github.com>
2020-08-06 23:29:37 -0500

    eleventh commit

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
